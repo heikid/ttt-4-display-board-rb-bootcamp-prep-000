454289743b15c7449fa67b9712f84f854ac16038
Hazelin Ngan <hazelin.ngan@gmail.com>
2017-10-20 19:34:07 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board
+  cells = "   "
+  rows = [cells, "|", cells, "|", cells]
+  board = [rows, "-----------", rows, "-----------", rows]
+  return board
+end
